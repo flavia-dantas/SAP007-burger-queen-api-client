70c54a22eedebd8d665d1b8fb81a564992666726
lud-ic <mag.ludmila@gmail.com>
2022-05-10 18:09:05 -0300

    feat: WIP page register
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { Register } from "./pages/Register/register";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World
-        </p>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <p>Hello World</p>
+        </header>
+      </div>
+      <Register></Register>
+    </>
   );
 }
 
