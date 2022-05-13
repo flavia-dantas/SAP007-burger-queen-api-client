88f85bc6bf7829f35bbfd2085fe7b65de1c2aef3
lud-ic <mag.ludmila@gmail.com>
2022-05-13 20:45:56 -0300

    refactor: API request to createUser
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,15 +1,17 @@
 const BASE_URL = "https://lab-api-bq.herokuapp.com";
 
-export const createUser = async (name, email, password, role) => {
-  return await fetch(`${BASE_URL}/users`, {
+export const createUser = (name, email, password, role) => {
+  return fetch(`${BASE_URL}/users`, {
     method: "POST",
-    headers: {"Content-Type": "application/json"},
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       name: name,
       email: email,
       password: password,
       role: role,
-      restaurant: "Burguer Queen"
-    })
+      restaurant: "Burguer Queen",
+    }),
+  });
+};
   });
 };
