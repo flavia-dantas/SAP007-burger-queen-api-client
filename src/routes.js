d7795a93ac240553b8c3b798a4dab3e62ebd3164
lud-ic <mag.ludmila@gmail.com>
2022-05-14 18:27:14 -0300

    feat: add routes to menu, kitchen and order status
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { Register } from "./pages/Register";
 import { Login } from "./pages/Login";
+import { Menu } from "./pages/Menu";
+import { Kitchen } from "./pages/Kitchen";
+import { OrderStatus } from "./pages/OrderStatus";
 
 export const BurguerRoutes = () => {
   return (
@@ -8,6 +11,9 @@ export const BurguerRoutes = () => {
       <Routes>
         <Route exect path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/kitchen" element={<Kitchen />} />
+        <Route path="/order-status" element={<OrderStatus />} />
       </Routes>
     </BrowserRouter>
   );
