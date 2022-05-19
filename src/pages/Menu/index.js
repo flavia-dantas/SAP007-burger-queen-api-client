647c6093481fbba2684da97c91e54650af80cec3
flavia-dantas <flavia.dantas2@hotmail.com>
2022-05-18 22:44:35 -0300

    feat: button count items working
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -7,6 +7,7 @@ import { ButtonCountItems } from "../../components/ButtonCountItems";
 
 export const Menu = () => {
   const [menu, setMenu] = useState([]);
+  const [count, setCount] = useState(0);
 
   const filterMenu = (data, type) => {
     return data.filter((item) => item.type === type);
@@ -25,38 +26,50 @@ export const Menu = () => {
   const handleClickMenu = (e) => {
     return showProducts(e.target.value);
   };
-  console.log(menu);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCount = () => {
+    if(count > 0) {
+      setCount(count - 1);
+    }
+  };
 
   return (
     <>
       <p>MENU</p>
       <div className="container-main">
-      <Button
-        text="Café da Manhã"
-        value="breakfast"
-        onClick={handleClickMenu}
-      />
-      <Button
-        text="Almoço e Jantar"
-        value="all-day"
-        onClick={handleClickMenu}
-      />
-      <ul className="container-products">
-        {menu.map((item) => {
-          return (
+        <Button
+          text="Café da Manhã"
+          value="breakfast"
+          onClick={handleClickMenu}
+        />
+        <Button
+          text="Almoço e Jantar"
+          value="all-day"
+          onClick={handleClickMenu}
+        />
+        <ul className="container-products">
+          {menu.map((item) => {
+            return (
             <div key={item.id}>
-          <MenuCard
-            key={item.id}
-            image={item.image}
-            name={item.name}
-            price={item.price}
-          >
-            <ButtonCountItems key2={item.id}/>
-          </MenuCard>     
+              <MenuCard
+                image={item.image}
+                name={item.name}
+                price={item.price}
+              >
+                <ButtonCountItems
+                  amount={count}
+                  increase={increaseCount}
+                  decrease={decreaseCount}
+                />
+              </MenuCard>
             </div>
-          );
-        })}
-      </ul>
+            );
+          })}
+        </ul>
       </div>
     </>
   );
