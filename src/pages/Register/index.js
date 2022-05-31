1fd3eedf6f3d0050b5e91bfb5351780a1658a197
lud-ic <mag.ludmila@gmail.com>
2022-05-30 22:42:45 -0300

    refactor: register page
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,3 +1,4 @@
+import "./style.css";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../../components/Button";
@@ -40,6 +41,8 @@ export const Register = () => {
     <>
       <LayoutForm>
       <img className="logo" src={Logo} alt="logo"/>
+        <form className="container-form">
+          <h2 className="form-title">Cadastre-se</h2>
           <InputElement
             type="text"
             label="Nome"
@@ -64,9 +67,11 @@ export const Register = () => {
             placeholder="******"
             onChange={(e) => setPassword(e.target.value)}
           />
+          <div className="input-radio-container">
           <InputElement
             type="radio"
             className="input-radio"
+            classNameLabel="label-radio input-label"
             label="Atendente"
             value="saloon"
             name="role"
@@ -76,12 +81,14 @@ export const Register = () => {
           <InputElement
             type="radio"
             className="input-radio"
+            classNameLabel="label-radio input-label"
             label="Cozinha"
             value="kitchen"
             name="role"
             checked={role === "kitchen"}
             onChange={(e) => setRole(e.target.value)}
           />
+          </div>
           <MessageStatusCode
             disable={errorMessage ? false : true}
             message={errorMessage}
