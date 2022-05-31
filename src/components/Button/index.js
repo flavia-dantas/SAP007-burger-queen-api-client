f97fc5a16b94f2afb70242f2f8ffb5b7a06d8a84
lud-ic <mag.ludmila@gmail.com>
2022-05-30 22:27:54 -0300

    refactor: button component
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,15 @@
 import "./style.css";
 
-export const Button = ({ text, onClick, ...props }) => {
+export const Button = ({
+  text,
+  onClick,
+  classNameContainer = "button-container",
+  className = "button",
+  ...props
+}) => {
   return (
-    <div className="button-container">
-      <button className="button" onClick={onClick} {...props}>
+    <div className={classNameContainer}>
+      <button className={className} onClick={onClick} {...props}>
         {text}
       </button>
     </div>
