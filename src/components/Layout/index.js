24aecf27f560952c26b129dff2a0799629f22e66
lud-ic <mag.ludmila@gmail.com>
2022-05-30 22:29:43 -0300

    refactor: layout component
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 export const LayoutForm = ({children}) => {
   return (
     <div className="container">
-      <section className="container-form container-flexbox">
+      <section className="container-layout container-flexbox">
         <div className="container-content container-flexbox">
           {children}
         </div>
