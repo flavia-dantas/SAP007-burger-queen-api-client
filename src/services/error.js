565abe751823ee67da9cae93b054c18e904be078
flavia-dantas <flavia.dantas2@hotmail.com>
2022-06-03 19:51:19 -0300

    refactor: register page errors
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/services/error.js
+++ src/services/error.js
@@ -1,11 +1,16 @@
-export const statusCode = (response) => {
-  switch (response.status) {
+export const RegisterError = ({status}) => {
+  switch (status) {
     case 400:
       return "Preencha os dados obrigatórios";
     case 401:
       return "Usuário não autenticado";
     case 403:
       return "Email já cadastrado";
+    default:
+      return "Ocorreu um erro, tente novamente";
+  }
+};
+
     case 404:
       return "Usuário não encontrado";
     default:
