95f7caa4f2adff067eb819ba79d57117a32ad661
flavia-dantas <flavia.dantas2@hotmail.com>
2022-06-15 22:22:10 -0300

    refactor:  orders card style
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/components/OrdersCard/index.js
+++ src/components/OrdersCard/index.js
@@ -3,13 +3,25 @@ import "./style.css";
 export const OrdersCard = ({id, clientName, table, status, createdAt, updatedAt, products, children, preparationTime, processedAt}) => {
   return (
     <li className="orders-card">
+      <div className="orders-status">
+        {status}
+      </div>
+      <p className="orders-info"><span className="orders-info-span">Nº Pedido:</span> {id}</p>
+      <p className="orders-info"><span className="orders-info-span">Cliente:</span> {clientName}</p>
+      <p className="orders-info"><span className="orders-info-span">Mesa:</span> {table}</p>
+      <p className="orders-info"><span className="orders-info-span">Criado em:</span> {createdAt}</p>
       {status === "pronto" || status === "entregue" ?
       <>
-        <p>Processado: {processedAt}</p>
-        <p>Tempo de Preparo:{preparationTime}</p>
+        <p className="orders-info"><span className="orders-info-span">Preparado em:</span> {preparationTime}</p>
+        <p className="orders-info"><span className="orders-info-span">Finalizado em:</span> {processedAt}</p>
       </>
+      : ""
       }
-      <ul>Produtos: {products}</ul>
+      {status === "entregue" ?
+      <p className="orders-info"><span className="orders-info-span">Entregue em:</span> {updatedAt}</p>
+      : ""
+      }
+      <ul className="orders-info"><span className="orders-info-span">Produtos:</span> {products}</ul>
       {children}
     </li>
   );
