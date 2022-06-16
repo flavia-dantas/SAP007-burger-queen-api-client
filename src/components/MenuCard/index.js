34581dc72ae793d26fa18899b21289a515e77033
flavia-dantas <flavia.dantas2@hotmail.com>
2022-06-15 22:29:13 -0300

    fix: delete lost "$" on components
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/components/MenuCard/index.js
+++ src/components/MenuCard/index.js
@@ -14,7 +14,7 @@ export const MenuCard = ({
       <h3 className="item-name main-info">{name}</h3>
       {flavor === null ? "" : <h3 className="item-flavor main-info">{flavor}</h3>}
       {complement === null ?
-        "" : <h3 className="item-complement main-info">complemento: ${complement}</h3>}
+      "" : <h3 className="item-complement main-info">complemento: {complement}</h3>}
       <h4 className="item-price main-info">R${price},00</h4>
       {children}
     </li>
