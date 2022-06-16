3c650b6f5b38e94eb647b5788906e74afee7afde
flavia-dantas <flavia.dantas2@hotmail.com>
2022-06-15 22:34:19 -0300

    feat: orders delivery page
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,4 +1,5 @@
 export { hideErrorMessage } from "./hideErrorMessage.js";
 export { filterData } from "./filterData.js";
 export { sortData } from "./sortData.js";
+export { calculationPreparationTime, formatTime} from "./preparationTime.js";
 export { statusVerification} from "./statusVerification.js";
