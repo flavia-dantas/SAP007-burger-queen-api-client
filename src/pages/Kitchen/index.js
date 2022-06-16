23de45c5e73a4d41a4ac50d69d408ac175f3d90b
flavia-dantas <flavia.dantas2@hotmail.com>
2022-06-15 22:05:10 -0300

    refactor: useEffect on kitchen page
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/pages/Kitchen/index.js
+++ src/pages/Kitchen/index.js
@@ -4,9 +4,13 @@ import { OrdersCard } from "../../components/OrdersCard";
 import { ProductsOrder } from "../../components/ProductsOrder";
 import { getOrders, updateOrders } from "../../services/products";
 import { Header } from "../../components/Header";
-import { sortData } from "../../helper";
 import { Button } from "../../components/Button";
-import { calculationPreparationTime, formatTime } from "../../helper/preparationTime";
+import {
+  calculationPreparationTime,
+  filterData,
+  formatTime,
+  sortData,
+} from "../../helper";
 
 export const Kitchen = () => {
   const [orders, setOrders] = useState([]);
@@ -14,7 +18,11 @@ export const Kitchen = () => {
   useEffect(() => {
     getOrders()
     .then((response) => response.json())
-    .then((data) => setOrders(sortData(data)));
+    .then((data) => {
+      const sortDataKitchen = sortData(data);
+      const filterStatus = filterData(sortDataKitchen,"pending","preparing");
+      setOrders(filterStatus);
+    });
   }, []);
 
   useEffect(() => {
