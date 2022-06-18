c52022354a5570c791e8304180af7de6fc09bb65
lud-ic <mag.ludmila@gmail.com>
2022-06-17 21:22:26 -0300

    feat: orders history page
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/pages/OrdersHistory/index.js
+++ src/pages/OrdersHistory/index.js
@@ -1,9 +1,66 @@
+import "./style.css";
+import { useEffect, useState } from "react";
+import { OrdersCard } from "../../components/OrdersCard";
+import { ProductsOrder } from "../../components/ProductsOrder";
+import { getOrders } from "../../services/products";
 import { Header } from "../../components/Header";
+import {
+  calculationPreparationTime,
+  filterStatus,
+  formatTime,
+  sortData,
+  statusVerification
+} from "../../helper";
 
 export const OrdersHistory = () => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    getOrders()
+    .then((response) => response.json())
+    .then((data) => {
+      const sortOrders = sortData(data);
+      const filterData = filterStatus(sortOrders,"delivered");
+      setOrders(filterData);
+    });
+  }, []);
+
   return (
     <>
       <Header titlePage="Histórico dos Pedidos" />
+      <section className="orders-history-container">
+        <ul className="orders-container">
+          {orders.map((item) => {
+            return (
+              <div key={item.id}>
+                <OrdersCard
+                id={item.id}
+                clientName={item.client_name}
+                table={item.table}
+                status={statusVerification(item)}
+                status2={item.status}
+                createdAt={formatTime(item.createdAt)}
+                updatedAt={formatTime(item.updatedAt)}
+                processedAt={formatTime(item.processedAt)}
+                preparationTime={calculationPreparationTime(item.processedAt,item.createdAt)}
+                products={item.Products.map((element) => {
+                  return (
+                    <div key={element.id}>
+                      <ProductsOrder
+                      name={element.name}
+                      flavor={element.flavor}
+                      complement={element.complement}
+                      qtd={element.qtd}
+                      />
+                    </div>
+                  );
+                })}
+                />
+              </div>
+            );
+          })}
+        </ul>
+      </section>
     </>
   );
 };
