8fefd752a3c579df8521e466ce242a70647238b8
lud-ic <mag.ludmila@gmail.com>
2022-06-17 21:40:39 -0300

    refactor: change localStorage ID to role
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -1,8 +1,8 @@
 export const setToken = (token) => localStorage.setItem("token", token);
-export const setID = (id) => localStorage.setItem("id", id);
+export const setRole = (role) => localStorage.setItem("role", role);
 
 export const getToken = () => localStorage.getItem("token");
-export const getID = () => localStorage.getItem("id");
+export const getRole = () => localStorage.getItem("role");
 
 export const removeToken = () => localStorage.removeItem("token");
-export const removeID = () => localStorage.removeItem("id");
+export const removeRole = () => localStorage.removeItem("role");
