83c0cfc95b5116db2d584fbab919a552a229dcee
lud-ic <mag.ludmila@gmail.com>
2022-06-17 21:45:34 -0300

    feat: add header buttons to home, orders delivery and orders history pages
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,10 @@
 import "./style.css";
 import Logo from "../../assets/logo.svg";
 import Exit from "../../assets/exit.svg";
-import { getToken, removeToken } from "../../services/localStorage";
+import Delivery from "../../assets/delivery.svg";
+import History from "../../assets/history.svg";
+import Home from "../../assets/home.svg";
+import { getRole, getToken, removeRole, removeToken } from "../../services/localStorage";
 import { useNavigate } from "react-router-dom";
 
 export const Header = ({ titlePage }) => {
@@ -11,9 +14,24 @@ export const Header = ({ titlePage }) => {
   const handleLogout = () => {
     if(getToken){
       removeToken();
+      removeRole();
       return navigate("/");
     }
   };
+
+  const handleHome = () => {
+    if(getRole() === "saloon") {
+      navigate("/menu");
+    } else if(getRole() === "kitchen") {
+      navigate("/kitchen");
+    }
+  };
+
+  const handleHistory = () => navigate("/orders-history");
+
+  const handleDelivery = () => navigate("/orders-delivery");
+
+
 
   return (
     <header className="header">
@@ -22,9 +40,22 @@ export const Header = ({ titlePage }) => {
       </picture>
       <h1 className="header-title">{titlePage}</h1>
       <div className="buttons-container">
-        <button className="button-header" onClick={() => handleLogout()}>
-          <img src={Exit} />
+        <button className="button-header" onClick={handleLogout}>
+          <img className="img-button-header" src={Exit} />
         </button>
+        <div className="container-button-history-delivery">
+
+          <button className="button-header" onClick={handleHome}>
+            <img className="img-button-header" src={Home} />
+          </button>
+          {getRole() === "saloon" ?
+          <button className="button-header" onClick={handleDelivery}>
+            <img className="img-button-header" src={Delivery} />
+          </button> : "" }
+          <button className="button-header" onClick={handleHistory}>
+            <img className="img-button-header" src={History} />
+          </button>
+        </div>
       </div>
     </header>
   );
