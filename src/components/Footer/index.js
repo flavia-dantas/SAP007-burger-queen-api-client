b19780c617fcca7633c789c6a0f7f3cba8130ae2
lud-ic <mag.ludmila@gmail.com>
2022-06-17 23:06:38 -0300

    feat: footer
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+export const Footer = () => {
+  return (
+    <footer className="container-footer">
+      <p className="footer-text">Criado por <a className="text-link" target="_blank" href="https://github.com/flavia-dantas" rel="noreferrer">Flavia Almeida</a> e <a className="text-link" target="_blank" href="https://github.com/Lud-ic" rel="noreferrer">Ludmila Magalhães</a></p>
+    </footer>
+  );
+};
