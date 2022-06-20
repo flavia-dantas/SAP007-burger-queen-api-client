f2de927625e387c349720547c50527c9c9da67d9
lud-ic <mag.ludmila@gmail.com>
2022-06-20 18:04:23 -0300

    feat: allow access menu page only if is a saloon role
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: Lud-ic <mag.ludmila@gmail.com>

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -9,8 +9,10 @@ import { Header } from "../../components/Header";
 import { InputElement } from "../../components/Input";
 import { ErrorMessage } from "../../components/ErrorMessage";
 import { CreateOrderError } from "../../services/error";
-import {filterType, hideErrorMessage } from "../../helper";
+import { filterType, hideErrorMessage } from "../../helper";
 import { Modal } from "../../components/Modal";
+import { useNavigate } from "react-router-dom";
+import { getRole } from "../../services/localStorage";
 
 export const Menu = () => {
   const [menu, setMenu] = useState([]);
@@ -24,6 +26,7 @@ export const Menu = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [modal, setModal] = useState(false);
   const [modalSendOrder, setModalSendOrder] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     getProducts()
@@ -39,7 +42,9 @@ export const Menu = () => {
 
   const handleClickMenu = (e) => {
     setChangeColor(e.target.value);
-    e.target.value === "breakfast" ? setMenu(breakfastMenu) : setMenu(allDayMenu);
+    e.target.value === "breakfast"
+      ? setMenu(breakfastMenu)
+      : setMenu(allDayMenu);
   };
 
   const increaseCount = (item) => {
@@ -62,15 +67,15 @@ export const Menu = () => {
         order.splice(
           order.findIndex((element) => element.id === item.id),
           1
-          );
-          countElement.qtd = 0;
-        }
-        if (countElement.qtd > 1) {
-          countElement.qtd -= 1;
-        }
+        );
+        countElement.qtd = 0;
       }
-      setOrder([...order]);
-    };
+      if (countElement.qtd > 1) {
+        countElement.qtd -= 1;
+      }
+    }
+    setOrder([...order]);
+  };
 
   const getItemCount = (item) => {
     const findItem = order.find((element) => element.id === item.id);
@@ -81,8 +86,8 @@ export const Menu = () => {
     order.splice(
       order.findIndex((element) => element.id === item.id),
       1
-      );
-      setOrder([...order]);
+    );
+    setOrder([...order]);
   };
 
   useEffect(() => {
@@ -94,150 +99,168 @@ export const Menu = () => {
 
   const sendOrder = () => {
     createOrder(client, table, order)
-    .then((response) => {
-      if(response.status === 200){
-        setOrder([]);
-        setTable("");
-        setClient("");
-        return response.json();
-      }
-      setErrorMessage(CreateOrderError(response));
-    })
-    .catch(() => setErrorMessage(CreateOrderError({status:500})));
+      .then((response) => {
+        if(response.status === 200) {
+          setOrder([]);
+          setTable("");
+          setClient("");
+          return response.json();
+        }
+        setErrorMessage(CreateOrderError(response));
+      })
+      .catch(() => setErrorMessage(CreateOrderError({status: 500})));
     hideErrorMessage(setErrorMessage);
   };
 
   return (
     <>
-      <Header titlePage="Cardápio" />
-      <div className="container-main">
-        <section className="menu-section">
-          <div className="container-button">
-            <Button
-              className="button-menu button"
-              classNameContainer="button-container-right button-container "
-              value="breakfast"
-              onClick={handleClickMenu}
-              style={{
-                backgroundColor:
-                  changeColor === "breakfast" ? "#FF8601" : "#C16101",
-              }}
-            >Café da Manhã</Button>
-            <Button
-              className="button-menu button"
-              classNameContainer="button-container-left button-container"
-              value="all-day"
-              onClick={handleClickMenu}
-              style={{
-                backgroundColor:
-                  changeColor === "all-day" ? "#FF8601" : "#C16101",
-              }}
-            >Almoço e Jantar</Button>
-          </div>
-          <div className="container-menu">
-            <ul className="container-products">
-              {menu.map((item) => {
-                return (
-                  <div key={`order-${item.id}`}>
-                    <MenuCard
-                      image={item.image}
-                      name={item.name}
-                      flavor={item.flavor}
-                      complement={item.complement}
-                      price={item.price}
-                    >
-                      <ButtonCountItems
-                        amount={getItemCount(item)}
-                        increase={() => increaseCount(item)}
-                        decrease={() => decreaseCount(item)}
-                      />
-                    </MenuCard>
-                  </div>
-                );
-              })}
-            </ul>
-          </div>
-        </section>
-        <aside className="order-aside">
-          <section className="order-section">
-            <h2 className="order-title">Pedido</h2>
-            <div className="inputs-order">
-                <div className="input-client">
-                  <InputElement
-                    type="text"
-                    label="Nome do Cliente"
-                    value={client}
-                    name="input"
-                    placeholder="Nome do cliente"
-                    autoComplete="off"
-                    onChange={(e) => setClient(e.target.value)}
-                  />
-                </div>
-                <div className="input-table">
-                  <InputElement
-                    type="number"
-                    min="1"
-                    label="Mesa"
-                    value={table}
-                    name="input"
-                    placeholder="Nº"
-                    autoComplete="off"
-                    onChange={(e) => setTable(e.target.value)}
-                  />
-                </div>
+      {getRole() === "saloon" ? (
+        <>
+          <Header titlePage="Cardápio" />
+          <div className="container-main">
+            <section className="menu-section">
+              <div className="container-button">
+                <Button
+                  className="button-menu button"
+                  classNameContainer="button-container-right button-container "
+                  value="breakfast"
+                  onClick={handleClickMenu}
+                  style={{
+                    backgroundColor:
+                      changeColor === "breakfast" ? "#FF8601" : "#C16101",
+                  }}
+                >
+                  Café da Manhã
+                </Button>
+                <Button
+                  className="button-menu button"
+                  classNameContainer="button-container-left button-container"
+                  value="all-day"
+                  onClick={handleClickMenu}
+                  style={{
+                    backgroundColor:
+                      changeColor === "all-day" ? "#FF8601" : "#C16101",
+                  }}
+                >
+                  Almoço e Jantar
+                </Button>
               </div>
-            <ul className="items-container">
-              {order.map((item) => {
-                return (
-                  <div className="item-map" key={`order-${item.id}`}>
-                    <ItemCommand
-                      qtd={item.qtd}
-                      name={item.name}
-                      price={item.price}
-                      flavor={item.flavor}
-                      complement={item.complement}
-                      totalPriceItem={item.price * item.qtd}
-                      onClickDelete={() => setModal((previous) => !previous)}
-                    >
-                      <ButtonCountItems
-                        classNameButton="button-count-order"
-                        amount={getItemCount(item)}
-                        increase={() => increaseCount(item)}
-                        decrease={() => decreaseCount(item)}
-                      />
-                    </ItemCommand>
+              <div className="container-menu">
+                <ul className="container-products">
+                  {menu.map((item) => {
+                    return (
+                      <div key={`order-${item.id}`}>
+                        <MenuCard
+                          image={item.image}
+                          name={item.name}
+                          flavor={item.flavor}
+                          complement={item.complement}
+                          price={item.price}
+                        >
+                          <ButtonCountItems
+                            amount={getItemCount(item)}
+                            increase={() => increaseCount(item)}
+                            decrease={() => decreaseCount(item)}
+                          />
+                        </MenuCard>
+                      </div>
+                    );
+                  })}
+                </ul>
+              </div>
+            </section>
+            <aside className="order-aside">
+              <section className="order-section">
+                <h2 className="order-title">Pedido</h2>
+                <div className="inputs-order">
+                  <div className="input-client">
+                    <InputElement
+                      type="text"
+                      label="Nome do Cliente"
+                      value={client}
+                      name="input"
+                      placeholder="Nome do cliente"
+                      autoComplete="off"
+                      onChange={(e) => setClient(e.target.value)}
+                    />
                   </div>
-                );
-              })}
-            </ul>
-            <div className="total-order-container">
-              <p className="total-order">
-                <span>Total</span>
-                <span>R${totalPrice},00</span>
-              </p>
-              <ErrorMessage
-                disable={errorMessage ? false : true}
-                message={errorMessage}
-              />
-              <Button onClick={() => setModalSendOrder((previous) => !previous)} >Finalizar Pedido</Button>
-            </div>
-          </section>
-        </aside>
-        <Modal
-        modal={modal}
-        click={() => setModal(false)}
-        onClickYes={deleteItem}
-        onClickNo={() => setModal(false)}>
-          Você tem certeza que deseja excluir o produto?
-        </Modal>
-        <Modal
-        modal={modalSendOrder}
-        click={() => setModalSendOrder(false)}
-        onClickYes={sendOrder}
-        onClickNo={() => setModalSendOrder(false)}>
-          Deseja finalizar o pedido?
-        </Modal>
-      </div>
+                  <div className="input-table">
+                    <InputElement
+                      type="number"
+                      min="1"
+                      label="Mesa"
+                      value={table}
+                      name="input"
+                      placeholder="Nº"
+                      autoComplete="off"
+                      onChange={(e) => setTable(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <ul className="items-container">
+                  {order.map((item) => {
+                    return (
+                      <div className="item-map" key={`order-${item.id}`}>
+                        <ItemCommand
+                          qtd={item.qtd}
+                          name={item.name}
+                          price={item.price}
+                          flavor={item.flavor}
+                          complement={item.complement}
+                          totalPriceItem={item.price * item.qtd}
+                          onClickDelete={() =>
+                            setModal((previous) => !previous)
+                          }
+                        >
+                          <ButtonCountItems
+                            classNameButton="button-count-order"
+                            amount={getItemCount(item)}
+                            increase={() => increaseCount(item)}
+                            decrease={() => decreaseCount(item)}
+                          />
+                        </ItemCommand>
+                      </div>
+                    );
+                  })}
+                </ul>
+                <div className="total-order-container">
+                  <p className="total-order">
+                    <span>Total</span>
+                    <span>R${totalPrice},00</span>
+                  </p>
+                  <ErrorMessage
+                    disable={errorMessage ? false : true}
+                    message={errorMessage}
+                  />
+                  <Button
+                    onClick={() => setModalSendOrder((previous) => !previous)}
+                  >
+                    Finalizar Pedido
+                  </Button>
+                </div>
+              </section>
+            </aside>
+            <Modal
+              modal={modal}
+              click={() => setModal(false)}
+              onClickYes={deleteItem}
+              onClickNo={() => setModal(false)}
+            >
+              Você tem certeza que deseja excluir o produto?
+            </Modal>
+            <Modal
+              modal={modalSendOrder}
+              click={() => setModalSendOrder(false)}
+              onClickYes={sendOrder}
+              onClickNo={() => setModalSendOrder(false)}
+            >
+              Deseja finalizar o pedido?
+            </Modal>
+          </div>
+        </>
+      ) : (
+        navigate("/kitchen")
+      )}
     </>
   );
 };
